8f8c55a254a42ee20ddb65f391ced94d796e3a72
Brandon Benefield <bsquared18@gmail.com>
2018-02-24 15:59:03 -0500

    Initial Push

---
+++ BB-Carousel.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+  Plugin Name: BB Carousel
+  Plugin URI: https://www.bbenefield.com
+  Description: Easy to use image slide show plugin
+  Version: 1.0.0
+  Author: Brandon Benefield
+  Author URI: https://www.bbenefield.com
+  License: GPLv2 or later
+  Text Domain: BB-Carousel
+
+*/
+  
